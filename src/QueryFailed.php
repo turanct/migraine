11ded4c93856142254d14fab9509286672dccc41
Toon Daelman <toon.daelman@engagor.com>
2020-06-07 11:34:21 +0200

    Clean up some of the config files & code standard checking

--- src/QueryFailed.php
+++ src/QueryFailed.php
@@ -17,8 +17,11 @@ final class QueryFailed extends \Exception
      */
     private $connection;
 
-    public static function withMigrationData(string $message, string $migration = '', string $connection = ''): QueryFailed
-    {
+    public static function withMigrationData(
+        string $message,
+        string $migration = '',
+        string $connection = ''
+    ): QueryFailed {
         $queryFailed = new static($message);
         $queryFailed->migration = $migration;
         $queryFailed->connection = $connection;
